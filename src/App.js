8c61df490f10295fe1576ad8a4aef3976d6566e6
will14lee <will14lee@gmail.com>
2021-11-18 15:34:35 -0500

    Connected React to Ruby Backend

--- src/App.js
+++ src/App.js
@@ -4,46 +4,7 @@ import StartJourney from './components/StartJourney';
 import FirstPokemon from './components/FirstPokemon';
 import Farewell from './components/Farewell';
 import Home from './components/Home';
-import {useState, useEffect} from 'react';
-
-const CreatedPokemons = () => {
-    const [search, setSearch]= useState("")
-    const [pokemon, setPokemon] = useState([])
-    const [filteredChara, setFilteredChara]= useState([])
-    useEffect(() => {
-        fetch('http://localhost:9292/pokemons')
-        .then(resp => resp.json())
-        .then(data =>setPokemon(data))
-    },[])
-
-      function CharacterForm(char){
-        return(
-          <div>
-          <p>Name: {pokemon.name}</p>
-          <p>Type: {pokemon.pokemon_type}</p>
-          </div>
-          )
-        }
-        function handleOnChange(e){
-          setSearch(e.target.value)
-          setFilteredChara(pokemon.filter(({Name})=> Name.includes(search)))
-        }
-        
-        const renderCharacters = () => {
-          const charactersToRender = filteredChara.length > 0 ? filteredChara : pokemon
-          return charactersToRender.map(perChara=> CharacterForm(perChara))
-        }
-        return (
-          <div>
-            {/* <h3>My Pokemon ${lokemon}</h3> */}
-            {/* <input placeholder= "Search..." onChange={handleOnChange }></input><button>Submit</button> */}
-            { renderCharacters()} 
-        </div>
-    )
-  }
-
-
-
+// import {useState, useEffect} from 'react';
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,49 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
+
+
+const CreatedPokemons = () => {
+    const [search, setSearch]= useState("")
+    const [pokemon, setPokemon] = useState([])
+    const [filteredChara, setFilteredChara]= useState([])
+    useEffect(() => {
+        fetch('http://localhost:9292/pokemons')
+        .then(resp => resp.json())
+        .then(data =>setPokemon(data))
+    },[])
+
+      function CharacterForm(char){
+        return(
+          <div>
+          <p>Name: {char.name}</p>
+          <p>Type: {char.pokemon_type}</p>
+          </div>
+          )
+        }
+        function handleOnChange(e){
+          setSearch(e.target.value)
+          setFilteredChara(pokemon.filter(({Name})=> Name.includes(search)))
+        }
+        
+        const renderCharacters = () => {
+          const charactersToRender = filteredChara.length > 0 ? filteredChara : pokemon
+          return charactersToRender.map(perChara=> CharacterForm(perChara))
+        }
+        return (
+          <div>
+            {/* <h3>My Pokemon ${lokemon}</h3> */}
+            {/* <input placeholder= "Search..." onChange={handleOnChange }></input><button>Submit</button> */}
+            { renderCharacters()} 
+        </div>
+    )
+  }
+
 
 const Home = () => {
   return (
     <div className="App">
-    {/* <p>Ah. I see. A fine starter! Please cherish your first Pokemon. Train with it, fight with it, and have many adventures with it!</p>
-    <p>Now please be safe in your exploration of the world... Of Pokemon!!! </p>
-    <p><button>See ya!</button></p> */}
-    <h2>Previously made Characters!</h2>
-    {/* <p><CreatedPokemons/></p> */}
+    <h2>Previously made Pokemon!</h2>
+    <p><CreatedPokemons/></p>
     </div>
 );
 }
