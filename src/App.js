e3c1000bb3880259177a7fcd728216e8829481d8
will14lee <will14lee@gmail.com>
2021-11-19 03:05:42 -0500

    /

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Home from './components/Home'
 import CreatedPokemon from './components/CreatedPokemon'
 import CreatedPokemonTrainers from './components/CreatedPokemonTrainers'
-// import NewGame from './components/NewGame'
+import NewGame from './components/NewGame'
 // import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Hi from './components/Hi';
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Routes>
           <Route path= "/" element= { <Home/> }/>
-          <Route path= '/new_game' element= {<CreatedPokemon/>}/>
+          <Route path= '/new_game' element= {<NewGame/>}/>
           {/* <Route path= "/new_game" element= { <Home/> }/> */}
           {/* <Route path= "/new_game" element= { <Hi/> }/> */}
           <Route path= "/created_pokemon" element= { <CreatedPokemon />}/>
